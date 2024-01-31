3416dd32329f6490278f4b7c40c34f3208e395aa
wefixte <cassy.pernelle@gmail.com>
2024-01-31 11:50:40 +0100

    Remove spare space

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         index = 0
         for col in row:
-            print("{:d}".format(col), end=" ")
+            print("{:d}".format(col), end="")
             if index < len(row) - 1:
                 print(" ", end="")
             index += 1
