b964c5fc6379d07e107e56800793a0b98a7ac783
wefixte <cassy.pernelle@gmail.com>
2024-01-31 12:25:28 +0100

    Simplified the condition

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for index in my_list:
-        if my_list[index] % 2 == 0:
+        if index % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
