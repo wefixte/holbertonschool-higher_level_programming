44b62774e95562ea7ceff8359a9c7b7b9f5b8ec3
wefixte <cassy.pernelle@gmail.com>
2024-01-31 11:49:13 +0100

    Add ending print matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        index = 0
         for col in row:
             print("{:d}".format(col), end=" ")
+            if index < len(row) - 1:
+                print(" ", end="")
+            index += 1
         print()
