3fd0ac8ad1d0e92c6e2b1da591ff6ce7fca3610a
wefixte <cassy.pernelle@gmail.com>
2024-01-31 11:43:18 +0100

    Change to use str.format() methode

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for index in range(len(matrix)):
-        for index2 in range(len(matrix[index])):
-            print(matrix [index][index2], end=" ")
-        print()
+    print("\n".join(["".join(["{:d} ".format(item)
+                              for item in row]) for row in matrix]))
