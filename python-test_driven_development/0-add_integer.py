9fcbef7e5b9142f4e77283abb1ff5229e287e7a4
wefixte <cassy.pernelle@gmail.com>
2024-02-11 14:30:08 +0100

    Changed everything - tried something new

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,21 +10,17 @@ def add_integer(a, b=98):
     """
     Args:
         a: First value.
-        b: Second value : 98.
-
+        b: Second value. Defaults to 98.
     Raises:
         TypeError: If a or b is not an int or float.
-
     Returns:
         The addition of a and b.
     """
-    if isinstance(a, (int, float)):
-        a = int(a)
-    else:
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-
-    if isinstance(b, (int, float)):
-        b = int(b)
-    else:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
     return a + b
