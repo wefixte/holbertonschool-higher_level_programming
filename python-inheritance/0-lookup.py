b01859f2adc6fb5453b0cde042d7b38eecb04816
wefixte <cassy.pernelle@gmail.com>
2024-02-15 10:49:32 +0100

    Add "Lookup" Python program

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Define lookup function
+"""
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object 
+    Args :
+        obj: object to loop up
+    Return:
+        list of available attributes and methods
+    """
+    return dir(obj)
