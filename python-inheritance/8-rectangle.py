cb2800e71ca46b6d411ac751c0ddd16236705789
wefixte <cassy.pernelle@gmail.com>
2024-02-16 10:32:32 +0100

    Trying another way and fixed pycodestyle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,29 +1,11 @@
 #!/usr/bin/python3
 """
-Class BaseGeometry
+Class Rectangle that inherits from BaseGeometry
 """
 
 
-class BaseGeometry():
-    """ Represent class BaseGeometry """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    def area(self):
-        """ No implementation """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Validate parameter as a positive int
-        Args:
-            name: string, name of the parameter
-            value: int, parameter to validate
-        Raises:
-            TypeError: if value not an int
-            ValueError: if value negative
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """ Define a rectangle """
