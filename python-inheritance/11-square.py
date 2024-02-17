b6cc623e0265389b130a95b4f8e132fc3997f94e
wefixte <cassy.pernelle@gmail.com>
2024-02-17 14:49:30 +0100

    Add "Square #2" Python program

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Class Rectangle that inherits from BaseGeometry
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class Square that inherits from Rectangle """
+
+    def __init__(self, size):
+        """ Init a square
+        Args:
+            size: int, side size of square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ Returns the area of a square
+        Return:
+            square area (int)
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """ Returns string description of square """
+        return "[Square] {}/{}".format(self.__size, self.__size)
