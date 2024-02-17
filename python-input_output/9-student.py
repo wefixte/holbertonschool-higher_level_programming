b9d25a976ff17bd5f319a4b50e4d706208a4647d
wefixte <cassy.pernelle@gmail.com>
2024-02-17 22:25:36 +0100

    Add "Student to JSON" Python program

---
+++ python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" define class Student """
+
+
+class Student():
+    """ class Student """
+
+    def __init__(self, first_name, last_name, age):
+        """ initialize class Student
+        Args:
+            first_name (str): first name
+            last_name (str): last name
+            age (int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ return dictionary representation of Student """
+        return self.__dict__
