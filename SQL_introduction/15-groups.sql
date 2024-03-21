827005a689c3d3ff6f55fe3825f6548e9621a2ec
wefixte <cassy.pernelle@gmail.com>
2024-03-21 10:50:50 +0100

    TRying again t15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,2 @@
 -- Script that lists the number of records with same score
-SELECT score, COUNT(*) AS number FROM second_table 
-GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
