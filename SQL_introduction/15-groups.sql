2cf76908873eff7a708601d52bc5bf1a932a1aaf
wefixte <cassy.pernelle@gmail.com>
2024-03-21 10:45:40 +0100

    Fixed T15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- Script that lists the number of records with same score
-SELECT score, COUNT(number) FROM second_table
-GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(*) AS number FROM second_table 
+GROUP BY score ORDER BY number DESC;
